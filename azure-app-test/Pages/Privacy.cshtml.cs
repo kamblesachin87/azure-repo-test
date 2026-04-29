163cfa9250c10b704791ac9b454a795637a9bf86
SachinKamble <SachinKamble@IBM-PF349CWY>
2026-04-29 16:55:15 +0530

    Add project files.

---
+++ azure-app-test/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_test.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
